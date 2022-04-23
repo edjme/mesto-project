2bdb162907158826d3618d1fca480fa2313079b9
edjme <stasikparmut@yandex.ru>
2022-04-23 21:49:04 +0300

    Проверка

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,8 @@
 const config = {
-    url: "https://nomoreparties.co/v1/plus-cohort-8",
+    url: 'https://nomoreparties.co/v1/plus-cohort-8',
     headers: {
-        "Content-type": "application/json",
-        authorization: '997c9532-3a56-4a0c-8fd0-93697b0a5910'
+        authorization: '38053800-14c5-4c22-ac44-33381f46c6f6',
+        "Content-type": "application/json"
     },
 }
 
@@ -10,41 +10,32 @@ const onResponse = (res) => {
     return res.ok ? res.json() : Promise.reject(res)
 }
 
-export function getAllTasks() {
+export function getInitialCards() {
     return fetch(config.url)
         .then(onResponse)
 }
 
-export function addTasks() {
+export function addCard(data) {
     return fetch(config.url, {
         method: 'POST',
         headers: config.headers,
         body: JSON.stringify(data)
     })
         .then(onResponse)
-        // .then((data) => {
-        //     console.log(data)
-        // })
-        // // .catch(err => {
-        // //     console.log(err)
-        // //     return Promise.reject(err)
-        // // })
 }
 
-export function removeAllTasks(dataId) {
+export function removeCard(dataId) {
     return fetch(`${config.url}/${dataId}`, {
         method: 'DELETE',
-        headers: config.headers,
-        body: JSON.stringify(data)
     })
         .then(onResponse)
 }
 
-export function editTask(daraId) {
-    return fetch(`${config.url}/${dataId}`, {
-        method: 'PUT',
-        headers: config.headers,
-        body: JSON.stringify(data)
-    })
-        .then(onResponse)
-}
+// export function editTask(daraId) {
+//     return fetch(`${config.url}/${dataId}`, {
+//         method: 'PUT',
+//         headers: config.headers,
+//         body: JSON.stringify(data)
+//     })
+//         .then(onResponse)
+// }
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@
 import '../pages/index.css'
 
 //api
-import {getAllTasks, addTasks, removeAllTasks, editTask} from './api'
+import {getInitialCards, addCard, removeCard, editTask} from './api'
 
 // Подключение валидации
 import {enableValidation, validationConfig} from './validate.js'
@@ -54,38 +54,38 @@ form.addEventListener('submit', submitProfileForm);
 
 
 // РАБОТА С КАРТОЧКАМИ
-export const initialCards = [
-    {
-        title: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        title: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        title: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        title: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        title: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        title: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    },
-    {
-        title: 'Принцесса Мононоке',
-        link: 'https://i.pinimg.com/564x/61/dd/c8/61ddc8ad90c7411f7a0f421a46750fe3.jpg'
-    }
-]; 
+// export const initialCards = [
+//     {
+//         title: 'Архыз',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+//     },
+//     {
+//         title: 'Челябинская область',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+//     },
+//     {
+//         title: 'Иваново',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+//     },
+//     {
+//         title: 'Камчатка',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+//     },
+//     {
+//         title: 'Холмогорский район',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+//     },
+//     {
+//         title: 'Байкал',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+//     },
+//     {
+//         title: 'Принцесса Мононоке',
+//         link: 'https://i.pinimg.com/564x/61/dd/c8/61ddc8ad90c7411f7a0f421a46750fe3.jpg'
+//     }
+// ]; 
 
-initialCards.forEach(card => renderCard(cardList, createCard(card.title, card.link)))
+// initialCards.forEach(card => renderCard(cardList, createCard(card.title, card.link)))
 
 
 
@@ -106,9 +106,10 @@ addForm.addEventListener('submit', (event) => {
     inputLink.value = "";
 })
 
-getAllTasks()
-    .then((dataFromServer) => {
-        dataFromServer(dataFromServer, cardList, 'append')
+getInitialCards()
+    .then((initialCards) => {
+        // dataFromServer(dataFromServer, renderCard, 'append')
+        initialCards.forEach(card => renderCard(cardList, createCard(card.title, card.link)))
     })
 
     
